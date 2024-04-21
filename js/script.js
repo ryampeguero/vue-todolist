780ace4e1ac32fcc651ae8c2d2ac97d0b25bde6b
ryampeguero <ryampeguero@gmail.com>
2024-04-21 22:03:52 +0200

    feat: first milestone done also added scroll in case of overflow-y

--- js/script.js
+++ js/script.js
@@ -3,18 +3,34 @@ const {createApp} = Vue;
 createApp({
     data(){
         return {
-            todoContainer: [
+            todoCont: [
                 {
                     text: "Cosa da fare",
                     done: false,
-                }
+                },
+                {
+                    text: "Cosa da fare1",
+                    done: true,
+                },
+                {
+                    text: "Cosa da fare2",
+                    done: false,
+                },
+                {
+                    text: "Cosa da fare3",
+                    done: false,
+                },
+                {
+                    text: "Cosa da fare4",
+                    done: false,
+                },
             ]
 
         };
     },
 
     created(){
-        console.log(this.todoContainer[0]);
+        
     },
 
     mounted(){},
